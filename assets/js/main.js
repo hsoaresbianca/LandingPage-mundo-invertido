9e710851e7d8040d18c7b2af7f4c6cd8028c0f8c
Bianca Soares <122945054+hsoaresbianca@users.noreply.github.com>
2024-10-02 21:36:55 -0300

    starting javascript code

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function main() {
+    const txtName = document.getElementById('txtName')
+    const txtEmail = document.getElementById('txtEmail')
+    const txtLevel = document.getElementById('txtLevel')
+    const txtCharacter = document.getElementById('txtCharacter')
+
+    document.getElementById('btnSubscribe').addEventListener('click', async () => {
+        const subscribe = {
+            name: txtName.value,
+            email: txtEmail.value,
+            level: txtLevel.value,
+            character: txtCharacter.value
+        }
+    })
+}
